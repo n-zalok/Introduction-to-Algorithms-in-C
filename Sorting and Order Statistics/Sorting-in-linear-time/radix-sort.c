33a7e322d4af20347e9435a1c2ad1b02f403a7b3
n-zalok <noor-zalouk@localhost.localdomain>
2025-11-20 00:16:12 +0200

    radix sort

---
+++ Sorting and Order Statistics/Sorting-in-linear-time/radix-sort.c
@@ -0,0 +1,24 @@
+#include <math.h>
+
+struct current_digit {
+    int *arr;
+    int *digit;
+};
+
+int* radix_sort(int *A, int n, int d) {
+    int* digit_counting_sort(struct current_digit A, int n);
+
+    struct current_digit B;
+    int D[n];
+    B.arr = A;
+    B.digit = D;
+
+    for (int i=0; i<d; i++) {
+        for (int j=0; j<n; j++) {
+            B.digit[j] = ((int)(B.arr[j]/pow(10, i))) % 10;
+        }
+        B.arr = digit_counting_sort(B, n);
+    }
+
+    return B.arr;
+}

a51d77a8e5514bffebc9bf107046a5bd6c50a751
n-zalok <noor-zalouk@localhost.localdomain>
2025-11-20 21:14:07 +0200

    randomized select

---
+++ Sorting and Order Statistics/randomized-select.c
@@ -0,0 +1,20 @@
+int randomized_select(int *A, int p, int r, int i) {
+    int randomized_partition(int *A, int p, int r);
+
+    if (p == r) {
+        return A[p];
+    }
+
+    int q = randomized_partition(A, p, r);
+    int k = q - p + 1;
+
+    if (i == k) {
+        return A[q];
+    }
+    else if (i < k) {
+        return randomized_select(A, p, q-1, i);
+    }
+    else {
+        return randomized_select(A, q+1, r, i-k);
+    }
+}

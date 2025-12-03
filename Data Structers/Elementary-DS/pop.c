e97e36d465d1b4df5e158bb91e56162748764abd
n-zalok <noor-zalouk@localhost.localdomain>
2025-12-04 00:29:32 +0200

    stack

---
+++ Data Structers/Elementary-DS/pop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <errno.h>
+
+struct stack {
+    int* arr;
+    int size;
+    int top;
+};
+
+int pop(struct stack *S) {
+    int stack_empty(struct stack S);
+
+    if (stack_empty(*S)) {
+        perror("stack underflow");
+    }
+    else {
+        S->top -= 1;
+        return S->arr[S->top+1];
+    }
+}

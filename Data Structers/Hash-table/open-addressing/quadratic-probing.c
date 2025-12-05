f0744509cb525dffa33debb63847a23964c8acc7
n-zalok <noor-zalouk@localhost.localdomain>
2025-12-06 00:03:07 +0200

    open addressing

---
+++ Data Structers/Hash-table/open-addressing/quadratic-probing.c
@@ -0,0 +1,11 @@
+int quadratic_probing(int k, int i, int m) {
+    int hash(int k, int m);
+
+    int result = hash(k, m) + (i*i);
+    if (result < m) {
+        return result;
+    }
+    else {
+        return result - m;
+    }
+}

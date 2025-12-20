17f45d065ea42b4bd62d853e659a858f3693b246
n-zalok <nooreldeen2002@gmail.com>
2025-12-20 23:07:41 +0200

    adding stdlib.h to main files

--- Sorting and Order Statistics/Max-priority-queue/main-max-priority-queue.c
+++ Sorting and Order Statistics/Max-priority-queue/main-max-priority-queue.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 struct heap {
     int *arr;
--- Sorting and Order Statistics/Sorting-in-linear-time/radix-sort/main-radix-sort.c
+++ Sorting and Order Statistics/Sorting-in-linear-time/radix-sort/main-radix-sort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include <stdlib.h>
 
 int main() {
     srand(time(NULL));

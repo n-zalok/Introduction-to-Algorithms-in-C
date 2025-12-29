8da070908993da078aedfb3c4cb6d1a498792e77
n-zalok <nooreldeen2002@gmail.com>
2025-12-30 00:11:32 +0200

    depth-first-search

---
+++ Graph Algorithms/Depth-first-search/DFS.c
@@ -0,0 +1,73 @@
+// Note not all members of each structure required
+// for every application but we just unify the definition
+struct vertex {
+    int key;
+    int rank;  // to use union by rank in sets
+    int p;     // parent
+    int d;     // distance in BFS and discovery time in DFS
+    int f;     // finish time
+    char color;
+};
+
+struct edge {
+    int from;
+    int to;
+    int w;  // weight
+};
+
+struct graph {
+    struct vertex *V;
+    struct edge *E;
+};
+
+struct node_ll {
+    int key;
+    int w;           
+    struct node_ll *next;
+};
+
+// linked list (ll)
+struct ll {
+    struct node_ll *head;
+};
+
+
+int dfs_time = 0;
+
+void DFS(struct graph *G, int n, struct ll *Adj) {
+    #define NIL -1
+    void DFS_visit(struct graph *G, struct ll *Adj, int u);
+
+    for (int i=0; i<n; i++) {
+        G->V[i].color = 'w';
+        G->V[i].p = NIL;
+    }
+
+    for (int i=0; i<n; i++) {
+        if (G->V[i].color == 'w') {
+            DFS_visit(G, Adj, i);
+        }
+    }
+}
+
+void DFS_visit(struct graph *G, struct ll *Adj, int u) {
+    #define NULL ((void *)0)
+
+    dfs_time += 1;
+    G->V[u].d = dfs_time;
+    G->V[u].color = 'g';
+
+    struct node_ll *v = Adj[u].head;
+    while (v != NULL) {
+        if (G->V[v->key].color == 'w') {
+            G->V[v->key].p = u;
+            DFS_visit(G, Adj, v->key);
+        }
+
+        v = v->next;
+    }
+
+    G->V[u].color = 'b';
+    dfs_time += 1;
+    G->V[u].f = dfs_time;
+}

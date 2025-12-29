51cd3d5f1a471021bbf44e3716470571037ebc10
n-zalok <nooreldeen2002@gmail.com>
2025-12-29 22:38:48 +0200

    adjacency list

---
+++ Graph Algorithms/adjacency-list.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+
+// Note not all members of each structure required
+// for every application but we just unify the definition
+struct vertex {
+    int key;
+    int rank;  // to use union by rank in sets
+    int p;     // parent
+    int d;     // distance in BFS and discovery time in DFS
+    int f;     // finish time
+    char color;
+};
+
+struct edge {
+    int from;
+    int to;
+    int w;  // weight
+};
+
+struct graph {
+    struct vertex *V;
+    struct edge *E;
+};
+
+struct node_ll {
+    int key;
+    int w;           
+    struct node_ll *next;
+};
+
+// linked list (ll)
+struct ll {
+    struct node_ll *head;
+};
+
+
+struct ll* adjacency_list(struct graph *G, int n, int m, char type) {
+    void single_list_insert(struct ll *L, int key, int w);
+
+    struct ll *Adj = malloc(sizeof(struct ll) * n);
+    for (int i=0; i<n; i++) {
+        Adj[i].head = NULL;
+    }
+
+    if (type == 'd') {
+        for (int i=0; i<m; i++) {
+            single_list_insert(&Adj[G->E[i].from], G->E[i].to, G->E[i].w);
+        }
+    }
+    else {
+        for (int i=0; i<m; i++) {
+            single_list_insert(&Adj[G->E[i].from], G->E[i].to, G->E[i].w);
+            single_list_insert(&Adj[G->E[i].to], G->E[i].from, G->E[i].w);
+        }
+    }
+
+    return Adj;
+}
---
+++ Graph Algorithms/single-list-insert.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+struct node_ll {
+    int key;
+    int w;           
+    struct node_ll *next;
+};
+
+// linked list (ll)
+struct ll {
+    struct node_ll *head;
+};
+
+void single_list_insert(struct ll *L, int key, int w) {
+    #define NULL ((void *)0)
+
+    struct node_ll *x = (struct node_ll *)malloc(sizeof(struct node_ll));
+    x->key = key;
+    x->w = w;
+
+    x->next = L->head;
+    L->head = x;
+}
